3cfcac64181a07e2f2e54862eb2c5a4cd7e89d24
Thlipsi <121302871+Thlipsi@users.noreply.github.com>
2024-08-28 17:33:26 -0700

    defer

--- cookie.gd
+++ cookie.gd
@@ -3,11 +3,12 @@ extends TextureButton
 @export var pressed_color: Color
 @export var normal_color: Color
 
-func _ready():
-	set_deferred("self_modulate", normal_color)
-	
-func _on_pressed():
-	set_deferred("self_modulate", pressed_color)
-	await get_tree().create_timer(0.25).timeout
-	set_deferred("self_modulate", normal_color)
+
+func _on_button_down():
+	size = Vector2(100, 100)
+	global_position  += Vector2(14, 14)
 	GameManager.clicked_cookie(1)
+
+func _on_button_up():
+	size = Vector2(128, 128)
+	global_position  += Vector2(-14, -14)
--- game_manager.gd
+++ game_manager.gd
@@ -7,4 +7,3 @@ var cookie_bank: int = 0
 func clicked_cookie(cookie_count):
 	cookie_bank += cookie_count
 	emit_signal("update_cookie", cookie_count)
-	
