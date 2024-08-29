997b55a83eb916343ae7e6bf4d8d9f652473bd67
Thlipsi <121302871+Thlipsi@users.noreply.github.com>
2024-08-28 17:33:32 -0700

    Update game_manager.gd

--- game_manager.gd
+++ game_manager.gd
@@ -4,6 +4,12 @@ signal update_cookie
 
 var cookie_bank: int = 0
 
+var robot_farm_cookies: int = 10
+var robot_farm_level: int = 0
+
+func _process(delta):
+	pass
+
 func clicked_cookie(cookie_count):
 	cookie_bank += cookie_count
 	emit_signal("update_cookie", cookie_count)
